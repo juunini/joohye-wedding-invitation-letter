487933371c630076f47ce9c49152cfc765f548ae
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-07-09 13:38:42 +0900

    Install storybook

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-react-router-v6"
+  ],
+  "framework": "@storybook/react",
+  webpackFinal: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "src": path.resolve(__dirname, "../src"),
+    };
+  
+    return config;
+  },
+}
